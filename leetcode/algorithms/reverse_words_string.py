9ff07b1fa05ae2cbae392a448bf9877aaf61e9f0
tramontini <matheustramon@gmail.com>
2019-05-26 22:45:13 -0300

    Remove one line

--- leetcode/algorithms/reverse_words_string.py
+++ leetcode/algorithms/reverse_words_string.py
@@ -1,7 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         words_vec = s.split()
-        words_vec = words_vec[::-1]
 
-
-        return " ".join(words_vec)
+        return " ".join(words_vec[::-1])
